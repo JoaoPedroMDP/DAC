e44fcf3962d381774e9f185a3b45d6ff1828ec04
JoaoPedroMDP1 <dev.jpmpjp00@gmail.com>
2022-06-20 16:48:04 -0300

    [add] Modulo e componente Soma [change] chmod 766 em todo mundo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { SomaModule } from './soma/soma.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, SomaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/soma/soma.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SomaComponent } from './soma/soma.component';
+
+
+
+@NgModule({
+  declarations: [
+    SomaComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SomaModule { }
